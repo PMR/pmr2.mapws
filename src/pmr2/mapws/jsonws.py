46c0a4085b1480993773b09b3471578deb603ac7
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-08-05 19:00:09 +1200

    Map client forms for collection+json compatibility.

--- src/pmr2/mapws/jsonws.py
+++ src/pmr2/mapws/jsonws.py
@@ -1,10 +1,12 @@
 import json
 
-from pmr2.json.mixin import JsonPage, SimpleJsonFormMixin
+from pmr2.json.mixin import SimpleJsonFormMixin
+from pmr2.json.collection.mixin import JsonCollectionFormMixin
+from pmr2.json.collection.core import keyvalue_to_itemdata
 from pmr2.mapws.ricordo import QueryForm
 
 
-class QueryForm(SimpleJsonFormMixin, QueryForm):
+class SimpleJsonQueryForm(SimpleJsonFormMixin, QueryForm):
 
     def render(self):
         if self._searched:
@@ -17,3 +19,23 @@ class QueryForm(SimpleJsonFormMixin, QueryForm):
                 },
             } for v in self.results()]})
         return super(QueryForm, self).render()
+
+
+class CollectionQueryForm(JsonCollectionFormMixin, QueryForm):
+
+    def update(self):
+        super(CollectionQueryForm, self).update()
+        if self._searched:
+            self._jc_items = []
+            for v in self.results():
+                item = keyvalue_to_itemdata(v['data'])
+                item['data'].append({
+                    'name': 'source',
+                    'value': v['source'],
+                })
+                item['links'] = [{
+                    'href': v['source'] or v['obj'].getURL(),
+                    'prompt': v['obj'].Title or v['source'],
+                    'rel': 'bookmark',
+                }]
+                self._jc_items.append(item)
